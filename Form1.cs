05e4f7d23dc76d21f1929c1dfa47f298117b955b
ademirtug <ademirtug@gmail.com>
2022-11-04 11:28:14 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,369 @@
+﻿using System.Management;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace PCEnvanter
+{
+	public partial class Form1 : Form
+	{
+		ConcurrentBag<string> pcnamelist = new ConcurrentBag<string>();
+		ConcurrentBag<PC> pclist = new ConcurrentBag<PC>();
+
+		public Form1()
+		{
+			InitializeComponent();
+			List<string> prefixes = new List<string>() { "P71", "A71", "L71" };
+			GetPCList(prefixes);
+
+		}
+
+		private void GetPCList(List<string> prefixes)
+		{
+			PrincipalContext context = getPrincipalContext();
+
+			pcnamelist = new ConcurrentBag<string>();
+
+			foreach (string str in prefixes)
+			{
+				ComputerPrincipal queryFilter = new ComputerPrincipal(context);
+				queryFilter.Name = str + "*";
+				foreach (Principal principal in new PrincipalSearcher((Principal)queryFilter).FindAll())
+				{
+					if (principal is ComputerPrincipal computerPrincipal)
+						pcnamelist.Add(computerPrincipal.Name);
+				}
+			}
+
+
+			//foreach (string str in stringList)
+			//{
+			//	BackgroundWorker backgroundWorker = new BackgroundWorker();
+			//	backgroundWorker.DoWork += new DoWorkEventHandler(this.Worker_DoWork);
+			//	backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(this.Worker_RunWorkerCompleted);
+			//	backgroundWorker.RunWorkerAsync((object)str);
+			//}
+
+
+			//Parallel.ForEach(pcnamelist, pname =>
+			//{
+			//	PC pc = getPCInfo(pname);
+			//});
+			PC pc = getPCInfo("P71AKN");
+
+		}
+
+
+		private PC getPCInfo(string pcname)
+        {
+			PC pc = new PC() { Name = pcname };
+			var pt = getPCType(pc.Name);
+			pc.Model = pt[0];
+			pc.Manufacturer = pt[1];
+			pc.VideoCard = getVideoCard(pc.Name);
+			pc.User = getPCUser(pc.Name);
+			pc.IP = getPCIP(pc.Name);
+			pc.Enclosure = getPCEnclosure(pc.Name);
+			pc.Cpu = getCPU(pc.Name);
+			pc.Disk = getDisk(pc.Name);
+			pc.Memory = getMemory(pc.Name);
+			pc.Wei = getWei(pc.Name);
+			pc.Monitor = getMonitor(pc.Name);
+			pclist.Add(pc);
+			Debug.WriteLine(pc.Name + " Completed.");
+			return pc;
+		}
+
+		private string[] getPCType(string pcname)
+		{
+			string[] r = new string[] {"", ""};
+
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT Manufacturer, Model FROM Win32_ComputerSystem") )
+				{
+					r[0] = m["Model"]?.ToString() ?? "";
+					r[1] = m["Manufacturer"]?.ToString() ?? "";
+				}
+			}
+			catch (Exception ex)
+			{    
+			}
+			return r;
+
+		}
+		private VideoCard getVideoCard(string pcname)
+		{
+			VideoCard vc = new VideoCard();
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT * FROM Win32_VideoController"))
+				{
+					vc.Name = m["Name"]?.ToString() ?? "";
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+			return vc;
+
+		}
+
+		private Personnel? getPCUser(string pcname)
+		{
+			Personnel p = new Personnel();
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT Name, UserName FROM Win32_ComputerSystem") )
+				{
+					p.Name = m["Name"]?.ToString() ?? "";
+					p.UserName = m["UserName"]?.ToString()?.Replace("CSB\\", "") ?? "";
+
+					if (p.UserName.Length == 0)
+						return null;
+
+					using (PrincipalContext context = getPrincipalContext())
+					{
+						using (PrincipalSearcher principalSearcher = new PrincipalSearcher((Principal)new UserPrincipal(context) { SamAccountName = p.UserName }))
+						{
+							object ode = principalSearcher.FindOne()?.GetUnderlyingObject() ?? null;
+							if (ode == null)
+								return null;
+
+							DirectoryEntry? de = ode as DirectoryEntry;
+
+							p.Name = de.Properties["GivenName"]?.Value?.ToString() + " " + de.Properties["sn"]?.Value?.ToString();
+							p.Title = de.Properties["title"]?.Value?.ToString() ?? "";
+							de.Dispose();
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return null;
+			}
+			return p;
+
+		}
+		private string getPCIP(string pcname)
+		{
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = 'TRUE'"))
+				{
+					return m["IPAddress"] == null ? "0.0.0.0" : ((string[])m["IPAddress"])[0];
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+			return "0.0.0.0";
+
+		}
+		private string getPCEnclosure(string pcname)
+		{
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT ChassisTypes FROM Win32_SystemEnclosure"))
+				{
+					short[] numArray = m["ChassisTypes"] != null ? (short[])m["ChassisTypes"] : new short[1];
+					short num = numArray.Length != 0 ? numArray[0] : (short)3;
+					switch (num)
+					{
+						case 3:
+						case 4:
+						case 6:
+						case 7:
+							return "Masaüstü PC";
+						case 8:
+						case 9:
+						case 10:
+							return "Dizüstü Bilgisayar";
+						case 13:
+							return "Bütünleşik PC";    
+						default:
+							return num.ToString();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+			return "-";
+
+		}
+		
+		private CPU getCPU(string pcname)
+		{
+			CPU c = new CPU();
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT Name FROM Win32_Processor"))
+				{
+					//List<string> lst = new List<string>();
+					//foreach (var item in m.Properties)
+					//{
+					//    lst.Add(item.Name + ": " + item.Value?.ToString());
+					//}
+					c.Name = m["Name"]?.ToString() ?? "";
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+			return c;
+
+		}
+
+		private Monitor getMonitor(string pcname)
+		{
+			Monitor mo = new Monitor();
+
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT PNPDeviceID FROM CIM_DesktopMonitor"))
+				{
+					if (m["PNPDeviceID"] == null)
+						continue;
+
+					var parts = m["PNPDeviceID"].ToString().Split("\\");
+					if (parts.Length > 2)
+                    {
+						mo.ID = parts[1];
+						break;
+					}
+						
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+			mo.Size = GetMonitorSize(pcname);
+
+
+			return mo;
+
+		}
+		public double GetMonitorSize(string pcname)
+		{
+			try
+			{
+				double w = 0;
+				double h = 0;
+
+				foreach (ManagementObject m in Query(pcname, "SELECT * FROM WmiMonitorBasicDisplayParams", isMonitor: true))
+				{
+					w = Convert.ToDouble(m["MaxHorizontalImageSize"].ToString());
+					h = Convert.ToDouble(m["MaxVerticalImageSize"].ToString());
+				}
+				return Math.Sqrt((w * w) + (h * h)) / 2.54;
+			}
+            catch (Exception)
+            {
+
+            }
+			return 0;
+		}
+		private Disk getDisk(string pcname)
+		{
+			Disk d = new Disk();
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT * FROM MSFT_PhysicalDisk", true))
+				{
+					d.MediaType = m["MediaType"] != null ? m["MediaType"].ToString() : "0";
+					d.Capacity += m["Size"] != null ? Convert.ToUInt64(m["Size"].ToString()) : 1UL;
+				}
+			}
+			catch (Exception ex)
+			{
+			}
+			return d;
+
+		}
+
+		private Memory getMemory(string pcname)
+		{
+			Memory mm = new Memory();
+
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT * FROM Win32_PhysicalMemory"))
+				{
+					mm.Capacity += Convert.ToUInt64(m["Capacity"]?.ToString() ?? "0");
+					mm.Frequency = Convert.ToInt32(m["Speed"]?.ToString() ?? "0");
+					mm.MemoryTypeData = m["MemoryType"]?.ToString() ?? "";
+				}
+			}
+			catch (Exception)
+			{
+
+			}
+			return mm;
+		}
+		private Wei getWei(string pcname)
+		{
+			Wei w = new Wei();
+
+			try
+			{
+				foreach (ManagementObject m in Query(pcname, "SELECT * FROM Win32_WinSAT"))
+				{
+					w.Cpu = Convert.ToDouble(m["CPUScore"]?.ToString() ?? "0");
+					w.Disk = Convert.ToDouble(m["DiskScore"]?.ToString() ?? "0");
+					w.Graphics = Convert.ToDouble(m["GraphicsScore"]?.ToString() ?? "0");
+					w.Memory = Convert.ToDouble(m["MemoryScore"]?.ToString() ?? "0");
+				}
+			}
+			catch (Exception ex)
+			{
+
+			}
+			return w;
+		}
+
+
+		private PrincipalContext getPrincipalContext()
+		{
+#if DEBUG
+			return new PrincipalContext(ContextType.Domain, "csb.local", "akin.demirtug", "Sandalye22");
+#else
+			return new PrincipalContext(ContextType.Domain, "csb.local");
+#endif
+		}
+
+		private ManagementObjectCollection Query(string pcname, string q, bool isDisk = false, bool isMonitor = false)
+		{
+			string scope = "";
+
+			if (isDisk)
+				scope = "\\\\" + pcname + "\\root\\microsoft\\windows\\storage";
+			else if (isMonitor)
+				scope = "\\\\" + pcname + "\\root\\wmi";
+			else
+				scope = "\\\\" + pcname + "\\root\\CIMV2";
+
+			ManagementScope? ms = null;
+
+			if (Environment.MachineName != pcname)
+			{
+#if DEBUG
+				ConnectionOptions co = new ConnectionOptions();
+				co.Username = "csb\\akin.demirtug";
+                co.Password = "Sandalye22";
+				ms = new ManagementScope(scope, co);		
+#endif
+			}
+			
+			if(ms == null)
+				ms = new ManagementScope(scope);
+
+			ObjectQuery oq = new ObjectQuery(q);
+			ManagementObjectSearcher searcher = new ManagementObjectSearcher(ms, oq);
+			ManagementObjectCollection queryCollection = searcher.Get();
+			return queryCollection;
+		}
+	}
+}
---
+++ data.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PCEnvanter
+{
+	public class PC
+	{
+		public string Name { get; set; } = "";
+
+		public Personnel? User { get; set; }
+
+		public string IP { get; set; } = "";
+
+		public string Model { get; set; } = "";
+
+		public string Manufacturer { get; set; } = "";
+
+		public CPU? Cpu { get; set; }
+
+		public Disk? Disk { get; set; }
+
+		public Memory? Memory { get; set; }
+
+		public Monitor? Monitor { get; set; }
+
+		public Wei? Wei { get; set; }
+
+		public VideoCard? VideoCard { get; set; } 
+
+		public string Enclosure { get; set; } = "";
+
+		//public bool RetrieveInfo()
+		//{
+		//    var pt = getPCType(Name);
+		//    Model = pt[0];
+		//    Manufacturer = pt[1];
+		//    User = getPCUser();
+		//    IP = getPCIP();
+		//    Enclosure = getPCEnclosure();
+		//    Cpu = getCPU();
+		//    Disk = getDisk();
+		//    Memory = getMemory();
+		//    Wei = getWei();
+		//    var xe = getMonitor();
+		//    double size = GetMonitorSize();
+		//} 
+	}
+
+	public class Brand
+	{
+		public string Manufacturer { get; set; } = "";
+
+		public string Model { get; set; } = "";
+
+		public string Enclosure { get; set; } = "";
+	}
+
+	public class CPU
+	{
+		public CPU(){}
+		public CPU(string raw){}
+
+		public string Manufacturer 
+		{
+			get 
+			{
+				if (Name == "")
+					return "";
+
+				if (Name.ToUpper().Contains("INTEL"))
+					return "Intel";
+				else if (Name.ToUpper().Contains("AMD"))
+					return "AMD";
+
+				return "";
+			}
+
+		}
+
+		public string Model
+		{
+			get
+			{
+				if (Name == "")
+					return "";
+
+				string[] intel = { "i3", "i5", "i7", "i9" };
+                foreach (string processor in intel)
+                {
+					int index = Name.IndexOf(processor);
+					if (index > -1)
+						return Name.Substring(index, Name.IndexOf(" ", index) - index).Trim();
+				}
+
+				return Name;
+			}
+
+		}
+
+		public string Name { get; set; } = "";
+
+		public int Cores { get; set; } = 0;
+
+		public int Frequency { get; set; } = 0;
+	}
+
+	public class Memory
+	{
+		public ulong Capacity { get; set; } = 0;
+
+		public int Frequency { get; set; } = 0;
+
+		public string MemoryTypeData { get; set; } = "";
+
+		public string MemoryType
+		{
+			get
+			{
+				if (MemoryTypeData == "11")
+					return "Flash";
+				else if (MemoryTypeData == "17")
+					return "SDRAM";
+				else if (MemoryTypeData == "20")
+					return "DDR";
+				else if (MemoryTypeData == "21")
+					return "DDR2";
+				else if (MemoryTypeData == "22")
+					return "DDR2";
+				else if (MemoryTypeData == "23")
+					return "DDR2";
+				else if (MemoryTypeData == "24")
+					return "DDR3";
+				else if (MemoryTypeData == "26")
+					return "DDR4";
+				else if (MemoryTypeData == "0")
+					return Convert.ToInt32(Frequency) < 2133 || Convert.ToInt32(Frequency) > 5000 ? (Convert.ToInt32(Frequency) < 1066 || Convert.ToInt32(Frequency) >= 2133 ? (Convert.ToInt32(Frequency) >= 1066 ? "???" : "DDR2") : "DDR3") : "DDR4";
+
+				return "";
+			}
+		}
+
+
+		public override string ToString()
+		{
+			return (Capacity / (1024 * 1024 * 1024)) + " GB "+ MemoryType;
+		}
+	}
+
+	public class Wei
+	{
+		public double Cpu { get; set; } = 0;
+		public double Graphics { get; set; } = 0;
+		public double Disk { get; set; } = 0;
+		public double Memory { get; set; } = 0;
+		public double Overall 
+		{ 
+			get 
+			{
+				return 0;
+			} 
+		}
+
+	}
+
+	public class Disk
+	{ 
+		public ulong Capacity { get; set; } = 0;
+		public string MediaType { get; set; } = "";
+		public override string ToString()
+		{
+			string t = "";
+			if (MediaType == "3" || MediaType == "0")
+				t = "HDD";
+			else if (MediaType == "4")
+				t = "SSD";
+
+			return Capacity > 0 ? Capacity / 1000000000 + " GB " + t : t;
+		}
+	}
+
+	public class Personnel
+	{
+		public string Name { get; set; } = "";
+
+		public string UserName { get; set; } = "";
+
+		public string UniName
+		{
+			get
+			{
+				return Name.ToUpper().RemoveDiacritics();
+			}
+		}
+
+		public string Title { get; set; } = "";
+
+	}
+	public class Monitor
+	{
+		public string ID { get; set; } = "";
+
+		public double Size { get; set; } = 0;
+	}
+
+	public class VideoCard
+	{
+		public string Name { get; set; } = "";
+	}
+
+	public static class MyExtensions
+	{
+		public static string RemoveDiacritics(this string text)
+		{
+			var normalizedString = text.Normalize(NormalizationForm.FormD);
+			var stringBuilder = new StringBuilder();
+
+			foreach (var c in normalizedString.EnumerateRunes())
+			{
+				var unicodeCategory = Rune.GetUnicodeCategory(c);
+				if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+				{
+					stringBuilder.Append(c);
+				}
+			}
+
+			return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+		}
+
+	}
+}
